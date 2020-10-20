aa3322c0f7f93d6fd8dc5a1e70cee580d9c56a89
ravi <ravi090694@gmail.com>
2020-10-20 17:14:47 +0530

    committing changed file

--- currency-exchange-service/currency-exchange-service/src/main/java/com/example/currency/exchange/service/CurrecncyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/example/currency/exchange/service/CurrecncyExchangeController.java
@@ -1,15 +1,20 @@
 package com.example.currency.exchange.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class CurrecncyExchangeController {
 
+	private static final  Logger LOGGER = LoggerFactory.getLogger(CurrecncyExchangeController.class);
+	
 	@Autowired
 	Environment env;
 	
@@ -21,6 +26,7 @@ public class CurrecncyExchangeController {
 		//ExchangeValue exchangeValue = new ExchangeValue(1000L,from,to,BigDecimal.valueOf(65));
 		ExchangeValue exchangeValue = currencyExchangeRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		LOGGER.info("{}",exchangeValue);
 		return exchangeValue;
 	}
 }
